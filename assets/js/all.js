1cf0812e3efec8beaceb4b73cf9e54756cacfe61
Cameron Van Orman <cameron.vanorman@gmail.com>
2016-12-31 16:44:16 -0800

    ^ js with npm for bootstrap

--- assets/js/all.js
+++ assets/js/all.js
@@ -39763,15 +39763,6 @@ var coerce = exports.coerce = {
 // 	el: 'body'
 // });
 
-$.fn.extend({
-    animateCss: function (animationName) {
-        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-        this.addClass('animated ' + animationName).one(animationEnd, function() {
-            $(this).removeClass('animated ' + animationName);
-        });
-    }
-});
-
 var FUSION = {
 
 	/**
--- assets/js/components/base.js
+++ assets/js/components/base.js
@@ -77,15 +77,6 @@
 // 	el: 'body'
 // });
 
-$.fn.extend({
-    animateCss: function (animationName) {
-        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-        this.addClass('animated ' + animationName).one(animationEnd, function() {
-            $(this).removeClass('animated ' + animationName);
-        });
-    }
-});
-
 var FUSION = {
 
 	/**
